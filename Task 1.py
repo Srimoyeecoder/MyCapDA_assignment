3ca2985e9f49e42b6db5a42b27a6b81872717c27
Srimoyeecoder <147419368+Srimoyeecoder@users.noreply.github.com>
2023-12-07 20:27:44 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 20:25:15 2023
+
+@author: SRIMOYEE
+"""
+
+| Student_ID | Study_Hours | Attendance | Test_Score |
+|------------|-------------|------------|------------|
+| 1          | 10          | 90         | 85         |
+| 2          | 8           | 80         | 78         |
+| 3          | 12          | 95         | 92         |
+| 4          | 9           | 88         | 80         |
+| 5          | 11          | 92         | 88         |
+#Explanatory Variables:
+#Study_Hours: The number of hours a student has studied.
+#Attendance: The percentage of classes attended by the student.
+#Response Variable:
+#Test_Score: The score a student achieved on a test.
